750354c14c77f08f6cae66e7c8b9a48cd1668a66
Ben Ebsworth <ben.ebsworth@gmail.com>
2018-03-02 18:44:41 +1100

    added basic db read/write example with an incrementer

--- demo/app/models.py
+++ demo/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+
+class RandomContent(models.Model):
+    count = models.IntegerField()
--- demo/app/views.py
+++ demo/app/views.py
@@ -1,10 +1,40 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import RandomContent
+
+
+def get_count():
+    """
+    Fetch the latest count value
+    :return:
+    """
+    obj = RandomContent.objects.first()
+    if obj:
+        return obj.count
+    else:
+        return 0
+
+
+def increment_count():
+    """
+    Increment count on each click
+    :return:
+    """
+    obj = RandomContent.objects.first()
+    if obj:
+        obj.count += 1
+        obj.save()
+        return True
+    else:
+        return False
 
 
 # Create your views here.
 def index(request):
     context = {
-        'message': 'example1',
+        'count': get_count(),
     }
+    print(request.POST)
+    if request.method == 'POST' and 'increment' in request.POST:
+        increment_count()
     return render(request, 'base_app.html', context)
